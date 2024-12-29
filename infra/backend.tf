c22f518c5a4023cec32276f7fee1e78500d15a4c
Matanmoshes <matanmoshes66@gmail.com>
2024-12-29 16:55:18 +0200

    new version

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,10 +3,10 @@
 ########################################################
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-djshfjsdbh"    # CHANGE to your S3 bucket name
+    bucket         = "my-terraform-state-bucket-v2-dfgfd"    # CHANGE to your S3 bucket name
     key            = "weatherapp-eks/terraform.tfstate"
     region         = "us-east-1"                    # CHANGE to your region
-    dynamodb_table = "my-terraform-lock-table-dfhsjdf"      # CHANGE to your DynamoDB table name
+    dynamodb_table = "my-terraform-lock-table"      # CHANGE to your DynamoDB table name
     encrypt        = true
   }
 }
